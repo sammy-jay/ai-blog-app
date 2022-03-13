a7b9782729ebdfc109906773aa67cecaa90632d9
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 00:10:17 +0100

    3 files modified, 5 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import './style.css';
 
 export default function App() {
+  const [newsArticles, setNewArticles] = useState([]);
+  useEffect(() => {
+    alanBtn({
+      key: 'ea78d0fb66a064e68b8721451295c4c92e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          setNewArticles(articles);
+        }
+      },
+    });
+  }, []);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Alan AI Blog Application</h1>
     </div>
   );
 }
